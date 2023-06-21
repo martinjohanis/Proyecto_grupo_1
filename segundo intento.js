21be371395bdd1d7d991346a247c77cd93136a8b
Grupo 1 <martin.johanis@escuelacangallo.edu.ar>
2023-06-21 09:08:01 -0300

    un par de avances

--- segundo intento.js
+++ segundo intento.js
@@ -1,48 +1,53 @@
-const perroalfa = [-3,-2,3,2,1]
-const perrobeta = [-1,-1,2,3,-3]
+const prompt = require('prompt-sync')();
+
+const perroalfa = [-3, -2, 3, 2, 1]
+const perrobeta = [-1, -1, 2, 3, -3]
 
 console.log("quiz")
 
-var valUser = [0,0,0,0,0]
+let valUser = [0,0,0,0,0]
 
-var tempStats = [0,0,0,0,0]
+let tempStats = [0,0,0,0,0]
 
-var quizActive = true
+let quizActive = true
 
-var questionState = 0;
+let questionState = 0;
 
-var questionText = [
-        "Te gusta mas la uno o la dos?",
-        "Te gusta mas la uno o la dos?",
-        "Te gusta mas la uno o la dos?"
-    ]
-
-var answerValues = [
-
-    [//preg 1
-      [2,0,-1,3,0] // opcion 1
-      [-1,-2,-3,0,0] //opcion 2
-    ]
-    
-    [//preg 2
-      [3,0,-3,-2,1] //opcion 1
-      [0,0,1,2,-3] //opcion 2
-    ]
-    
-    [//preg 3
-      [1,3,3,0,0]//opcion 1
-      [2,2,0,1,0]//opcion 2
-    ]
+let questionText = [
+  "Te gusta mas la uno o la dos?",
+  "Te gusta mas la uno o la dos?",
+  "Te gusta mas la uno o la dos?"
 ]
 
+let answerValues = [
+  [ //preg 1
+    [2,0,-1,3,0],   //opcion 1
+    [-1,-2,-3,0,0]  //opcion 2
+  ],
+  [ //preg 2
+    [3,0,-3,-2,1],  //opcion 1
+    [0,0,1,2,-3]    //opcion 2
+  ],
+  [ //preg 3
+    [1,3,3,0,0],    //opcion 1
+    [2,2,0,1,0]     //opcion 2
+  ]
+]
 
-
+function main(){
+  for(let i = 0; i < questionText.length; i++){
+    console.log("asfdasf")
+    responder()
+    addValues()
+    console.log(valUser)
+  }
+}
 
 
 function addValues() {
     
-  for (i = 0; i < userStats.length ; i++) {
-      userStats[i] += tempStats[i];
+  for (i = 0; i < valUser.length ; i++) {
+      valUser[i] += tempStats[i];
   }
 }
 
@@ -50,11 +55,11 @@ function responder(){
   
   clearTempStats()
 
-  for (let i = 0; i < answerValues.length; i++) {
+  for (let i = 0; i < questionText.length; i++) {
     
-    resp = prompt(questionText[i])
+    const resp = prompt(questionText[i]);
 
-    tempStats = answerValues[i-1][resp]
+    tempStats = answerValues[i-1],[resp]
     
     if (quizActive){
       questionState++
@@ -68,3 +73,4 @@ function clearTempStats() {
   tempStats = [0,0,0,0,0];	
 }
 
+main()
