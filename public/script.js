baaa509e2e559e86eefc81547f38287fab1b1dd5
Grupo 1 <martin.johanis@escuelacangallo.edu.ar>
2023-08-02 08:15:05 -0300

    pulido de html

--- public/script.js
+++ public/script.js
@@ -1,23 +1,110 @@
 const btnradio1 = document.getElementById('btnradio1');
+const outputDiv = document.getElementById('output');
 //const nameInput = document.getElementById('nameInput');
 //const outputDiv = document.getElementById('output');
+const respuestasUser = [];
+
+function enviarResp() {
+  const form = document.getElementById('myForm');
+
+  console.log(form);
+  
+  const elements = form.querySelectorAll('input, select, value'); //obtiene todos los elementos del formulario
+
+
+
+  elements.forEach(element => {
+    if (element.type === 'checkbox' || element.type === 'radio') {
+      if (element.checked) {
+        respuestasUser.push(element.value);
+      }
+    } else {
+      respuestasUser.push(element.value);
+    }
+  });
+
+  if (respuestasUser.length !== answerValues.length) {
+    alert("Seleccionar una opción en todas las preguntas");
+  }
+
+  console.log(respuestasUser);
+}
+
+
+function click_boton_comenzar()
+{
+      //alert("hola kai como estas?")
+      document.getElementById("texto1").innerHTML=""
+      document.getElementById("texto1").innerHTML=`  <div id="botonesop1">
+     <div class="btn-group1" role="group" aria-label="Basic radio toggle button group">
+      <h4> Te gusta mas la 1 o la 2?</h4>
+      
+      
+      <input type="radio" class="btn-check1" name="btnradio1" id="btnradio1" autocomplete="off" checked>
+      <label class="btn btn-outline-primary1" for="btnradio1">opcion 1</label>
+   
+      <input type="radio" class="btn-check1" name="btnradio1" id="btnradio2" autocomplete="off">
+      <label class="btn btn-outline-primary1" for="btnradio1">opcion 2</label>
+    </div>
+    </div>
+  
+  
+    <div id="botonesop2">
+     <div class="btn-group2" role="group" aria-label="Basic radio toggle button group">
+      <h4> Te gusta mas la 1 o la 2?</h4>
+      
+      
+      <input type="radio" class="btn-check2" name="btnradio2" id="btnradio3" autocomplete="off" checked>
+      <label class="btn btn-outline-primary2" for="btnradio2">opcion 1</label>
+   
+      <input type="radio" class="btn-check2" name="btnradio2" id="btnradio4" autocomplete="off">
+      <label class="btn btn-outline-primary2" for="btnradio2">opcion 2</label>
+    </div>
+    </div>
+  
+  
+    <div id="botonesop3">
+     <div class="btn-group3" role="group" aria-label="Basic radio toggle button group">
+      <h4> Te gusta mas la 1 o la 2?</h4>
+      
+      
+      <input type="radio" class="btn-check3" name="btnradio3" id="btnradio5" autocomplete="off" checked>
+      <label class="btn btn-outline-primary3" for="btnradio3">opcion 1</label>
+   
+      <input type="radio" class="btn-check3" name="btnradio3" id="btnradio6" autocomplete="off">
+      <label class="btn btn-outline-primary3" for="btnradio3">opcion 2</label>
+    </div>
+    </div>
+  
+    <button type="button" id="submitBtn" onclick="click_boton_finalizar()" class="btn btn-outline-secondary"><h4>Descubre tu resultado!</h4></button>
+  `
+  }
+
+  
+  function click_boton_finalizar()
+  {
+      alert("hola kai como estas?")
+      document.getElementById("submitBtn").innerHTML= enviarResp()
+  }
+
 
 // Agrega un listener al botón de submit para que cuando se haga click se envíe el nombre ingresado al servidor
-btnradio1.addEventListener('click', () => {
+submitBtn.addEventListener('click', () => {
   //const name = nameInput.value; // Obtiene el nombre ingresado en el input
-  const name = "NOMBRE"
-  fetch('/greet', { // Envía el nombre ingresado al servidor mediante una petición POST
+
+  fetch('/obtenerPerro', {
     method: 'POST',
+    body: JSON.stringify({ respuestasUser }),
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name }) // El nombre se envía en el body de la petición
-  }) // Luego de enviar la petición, se espera a que el servidor responda
-    .then(response => response.json()) // Se convierte la respuesta a JSON
-    .then(data => { // Si la conversión fue exitosa, se obtiene el saludo y se muestra en pantalla
-      outputDiv.textContent = data.greeting;
-    })
-    .catch(error => { // Si hubo un error, se muestra en consola
-      console.error('Error:', error);
-    });
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    outputDiv.textContent = data.resultado;
+  })
+
+  .catch(error => {
+    console.error('Error:', error);
+  });
 });
--- server/index.js
+++ server/index.js
@@ -1,44 +1,122 @@
 const express = require('express');
 const app = express();
 const port = 3000; // Puerto de la APP Web
+const prompt = require('prompt-sync')();
 
-// Código para importar el módulo 'bd' que maneja la conexión y las consultas a la BBDD (Está comentado para que no falle si no está corriendo el servidor MySQL)
-//const db = require('./../database/db');
+// Perros
+let perro1 = {
+  raza: "Golden Retriever",
+  values: [3,3,3,3,3],
+}
+let perro2 = {
+  raza: "Border Collie",
+  values: [-2,-2,-2,-2,-2],
+}
+let perro3 = {
+  raza: "Weimaraner",
+  values: [0,0,0,0,0], 
+}
+let perros = [perro1, perro2, perro3]//valores perros(perro = [indice.values])
+
+let valUser = [0,0,0,0,0]
+
+let answerValues = [
+  [ //preg 1
+    [2,0,-1,3,0],   //opcion 1
+    [-1,-2,-3,0,0]  //opcion 2
+  ],
+  [ //preg 2
+    [3,0,-3,-2,1],  //opcion 1
+    [0,0,1,2,-3]    //opcion 2
+  ],
+  [ //preg 3
+    [1,3,3,0,0],    //opcion 1
+    [2,2,0,1,0]     //opcion 2
+  ]
+]
+
 
 app.use(express.static(__dirname + "/../public")); // Indica que la carpeta 'public' contiene los archivos estáticos de la aplicación web
 app.use(express.json());  // Indica que se usarán datos en formato JSON en las peticiones
 
 // Ruta para obtener el saludo, recibe el nombre en el body de la petición y devuelve el saludo en formato JSON
-app.post('/greet', (req, res) => { 
-  const { name } = req.body;
+app.post('/obtenerPerro', (req, res) => { 
 
-  const greeting = `Hello, ${name}!`;
+  const respuestas = req.body
 
-  res.json({greeting});
-
+  guardarRespuestas()
+  
+  console.log(respuestas)
+  
   console.log("tumamam")
-  // Código para obtener el saludo usando los datos guardados en la BBDD
-  // Ustedes deben reemplazar este código por el suyo, según las necesidades de su aplicación
-  /* 
-  db.returnGreetingFromDB(name)
 
-    .then((greeting) => {
-      if (greeting) {
-        console.log(greeting);
-        res.json({ greeting });
-      } else {
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    })
 
-    .catch((error) => {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    });
-  */
+
+  /*for(let i = 0; i < valUser.length; i++){
+    addValues()
+  }*/
+
+  const resultado = calcularDistancia(perros, valUser);
+  const menorDistancia = resultado.subarreglo;
+  const perroMenorDistancia = resultado.perro;
+
+  res.json({ resultado: resultado.perro });
+
 });
 
 // Inicializa la aplicación web e imprime un mensaje en la consola indicando el puerto de escucha
 app.listen(port, () => { 
   console.log(`Server running on http://localhost:${port}`);
 });
+
+app.get('/',function(req,res){
+  res.set({
+      'perroResp': '*'
+      });
+  return res.redirect('index.html');
+  })
+
+  
+function guardarRespuestas() {
+
+  const respuestasUser = document.querySelectorAll('input[type="radio"]:checked');
+
+  if (respuestasUser.length === answerValues.length) {
+    respuestasUser.forEach((respuestas, index) => {
+      respuestas.push(answerValues[index][respuestas.value]);
+    });
+
+    console.log(respuestas); // respuestas seleccionadas por el usuario
+
+  } else {
+    alert("Debes seleccionar una opción para cada pregunta.");
+  }
+}
+
+function addValues() {
+  for (let i = 0; i < answerValues.length ; i++) {
+   for (let j = 0; j < valUser.length ; j++) {
+       valUser[j] += answerValues[i][opcionesArr[i]-1][j];
+   }
+  }
+}
+
+function calcularDistancia(perros, valUser) {  // calcula las distancias entre cada subarreglo y el valUser
+  
+  let distancias = [];//arreglo que contiene las distancias calculadas
+
+  for (let i = 0; i < perros.length; i++) {
+    let sumaDistancia = 0;
+    for (let j = 0; j < valUser.length; j++) {
+      sumaDistancia += Math.abs(perros[i].values[j] - valUser[j]); //calculo de distancias
+    }
+    distancias.push(sumaDistancia);//se agrega el numero del calculo al arreglo "distancias"
+  }
+
+
+  let indiceMenorDistancia = distancias.indexOf(Math.min(...distancias));  
+  // encuentra el subarreglo con el numero mas bajo en las sumas de distancias
+  let menorDistancia = perros[indiceMenorDistancia].values;
+  let perroMenorDistancia = nombres[indiceMenorDistancia].raza;
+  return { subarreglo: menorDistancia, perro: perroMenorDistancia };
+}
